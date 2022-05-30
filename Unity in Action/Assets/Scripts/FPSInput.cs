b3953b7c4fbaa437bcabf63c2ef6f589897a1031
Sertac <sertac@gmail.com>
2022-05-29 21:46:40 -0400

    Added stairs in middle to test climbing/colliding/falls with our charactercontroller
    Added stairs in middle to test climbing/colliding/falls with our charactercontroller

--- Unity in Action/Assets/Scripts/FPSInput.cs
+++ Unity in Action/Assets/Scripts/FPSInput.cs
@@ -6,12 +6,28 @@ public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
 
-    // Update is called once per frame
+    // variable for referencing the CharacterController
+    private CharacterController charController;
+
+    private void Start()
+    {
+        // access the CharacterController attached to this object
+        charController = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         // horizontal and vertical are indirect names for keyboard mappings
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        // limit diagonal movement to the same speed as movement along an axis
+        movement = Vector3.ClampMagnitude(movement, speed);
+        
+        // tell the CharacterController to move by that vector
+        charController.Move(movement);
+        //transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);       
+                
     }
 }
