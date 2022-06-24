23bc96d2b2861bbb85bc81eb38201a070959f13d
Esfekia <sertac@gmail.com>
2022-06-24 09:37:45 -0400

    Used PlayerPrefs to get/set the value of speed for future play sessions.
    Used PlayerPrefs to get/set the value of speed for future play sessions.

--- Unity in Action/Assets/Scripts/SettingsPopup.cs
+++ Unity in Action/Assets/Scripts/SettingsPopup.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour
 {
+    [SerializeField] Slider speedSlider;
+    private void Start()
+    {
+        // get command has both the value to get, as well as the default value
+        // in case speed was not saved previously.
+        speedSlider.value = PlayerPrefs.GetFloat("Speed", 1);
+    }
+
+
     // turn the object on to open the window
     public void Open()
     {
@@ -28,6 +38,9 @@ public class SettingsPopup : MonoBehaviour
     public void OnSpeedValue(float speed)
     {
         Debug.Log($"Speed: {speed}");
+
+        // save it for future play sessions
+        PlayerPrefs.SetFloat("Speed", speed);
     }
 
 }
