a8c1e602c67d779a6ff29315db4f7e4fd9e2e3b2
Esfekia <sertac@gmail.com>
2022-06-24 10:23:25 -0400

    Added a SPEED_CHANGED listener in SceneController and used it to update the instantiated Enemy GameObject's WanderingAI component's speed value with the updated value from the UI settings. The speed of the enemy now is updated even after death and instantiation of a new one.
    Added a SPEED_CHANGED listener in SceneController and used it to update the instantiated Enemy GameObject's WanderingAI component's speed value with the updated value from the UI settings. The speed of the enemy now is updated even after death and instantiation of a new one.

--- Unity in Action/Assets/Scripts/SceneController.cs
+++ Unity in Action/Assets/Scripts/SceneController.cs
@@ -9,7 +9,27 @@ public class SceneController : MonoBehaviour
         
     // private variable to keep track of the enemy instances in the scene
     private GameObject enemy;
-    
+
+    public float newSpeed = 3.0f;
+    // base speed that is adjusted by the speed setting in the settings menu
+    public const float baseSpeed = 3.0f;
+
+    private void OnEnable()
+    {
+        // supporting a value in the listener is as simple as adding a type definition, such as the <float> below.
+        Messenger<float>.AddListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+    }
+
+    private void OnDisable()
+    {
+        Messenger<float>.RemoveListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+    }
+
+    private void OnSpeedChanged(float value)
+    {
+        newSpeed = baseSpeed * value;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -22,6 +42,7 @@ public class SceneController : MonoBehaviour
             enemy.transform.position = new Vector3(-5, 15, 0);
             float angle = Random.Range(0, 360);
             enemy.transform.Rotate(0, angle, 0);
+            enemy.GetComponent<WanderingAI>().speed = newSpeed;
         }
 
     }
